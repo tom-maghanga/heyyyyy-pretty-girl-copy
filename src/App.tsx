1c6c77ac8218e6e383dbb6216c5f253d906688bd
titanium-dev <akinkunmioye42@gmail.com>
2025-02-02 09:28:55 +0100

    initialize React + TypeScript + Vite project with basic setup and configuration

---
+++ src/App.tsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
+import { motion, AnimatePresence } from "framer-motion";
+
+function App() {
+  const steps = [
+    {
+      content: "Heyyyyy, pretty girl.",
+      image: "/character/one.png",
+    },
+    {
+      content: `Recently, we met.
+      And somehow, you've been on my mind ever since.
+      `,
+      image: "/character/two.png",
+    },
+    {
+      content: `Then we went on our first date…And I realized—yep, I want this girl. For life.
+      `,
+      image: "/character/three.png",
+    },
+    {
+      content: `You're beautiful, you're smart, you're fun,
+and you make spending time together feel too short.`,
+      image: "/character/four.png",
+    },
+    {
+      content: `I look forward to when I'll see you again,
+hold your hands, and look into your pretty eyes.`,
+      image: "/character/five.png",
+    },
+    {
+      content: "So now I've got a question for you…",
+      image: "/character/six.png",
+    },
+    {
+      content: "Will you be my Valentine?",
+      image: "/character/seven.png",
+    },
+  ];
+  const [currentStep, setCurrentStep] = useState(0);
+  const [sheWantsToBeMyValentine, setSheWantsToBeMyValentine] = useState(false);
+  const { width, height } = useWindowSize();
+
+  return (
+    <>
+      {sheWantsToBeMyValentine && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Confetti width={width} height={height} />
+          <div className="fixed top-0 left-0 w-full h-full bg-[#FFC5D3] flex flex-col items-center justify-center">
+            <motion.h1
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ delay: 0.3, type: "spring" }}
+              className="text-white text-4xl font-bold"
+            >
+              Yayyyyyyy!!!!!
+            </motion.h1>
+            <img
+              src="/character/yayyyy.png"
+              alt=""
+              className="w-40 animate-bounce"
+            />
+          </div>
+        </motion.div>
+      )}
+      <div className="bg-[#FFC5D3] min-h-screen text-white p-5 flex flex-col items-center justify-center">
+        <motion.img
+          key={currentStep}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          src={steps[currentStep].image}
+          alt=""
+          className="w-40"
+        />
+        <motion.div
+          key={currentStep + "-text"}
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="font-josefin text-4xl font-bold"
+        >
+          {steps[currentStep].content}
+        </motion.div>
+
+        {currentStep < 6 && (
+          <>
+            <button
+              onClick={() => setCurrentStep(currentStep + 1)}
+              className="bg-white text-[#FFC5D3] py-3 text-xl rounded-xl w-full mt-10 font-semibold"
+            >
+              Next
+            </button>
+            {currentStep > 0 && (
+              <button
+                onClick={() => setCurrentStep(currentStep - 1)}
+                className="bg-white text-[#FFC5D3] py-3 text-xl rounded-xl w-full mt-2 font-semibold opacity-90"
+              >
+                Back
+              </button>
+            )}
+          </>
+        )}
+        {currentStep === 6 && (
+          <>
+            <button
+              onClick={() => setSheWantsToBeMyValentine(true)}
+              className="bg-white text-[#FFC5D3] py-3 text-xl rounded-xl w-full mt-10 font-semibold"
+            >
+              Yes
+            </button>
+
+            <button
+              onClick={() => setSheWantsToBeMyValentine(true)}
+              className="bg-white text-[#FFC5D3] py-3 text-xl rounded-xl w-full mt-2 font-semibold"
+            >
+              Yes
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
